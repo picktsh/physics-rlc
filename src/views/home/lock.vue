b841e96aa2621deedc90872e4a4b132cf1bd3f22
dear <dear@2up.io>
2026-07-14 01:43:25 +0800

    feat(auth): 优化验证码输入组件功能: 限制只允许数字, 默认打开数字键盘;

--- src/views/home/lock.vue
+++ src/views/home/lock.vue
@@ -1,8 +1,17 @@
 <script setup>
+import { onMounted, useTemplateRef } from 'vue'
 import { NCard, NInputOtp } from 'naive-ui'
 import { useAppStore } from '../../stores/app'
 
 const appStore = useAppStore()
+const otpWrapRef = useTemplateRef('otpWrapRef')
+
+onMounted(() => {
+  // 补偿 NInputOtp 缺失传递 inputProps.inputmode 属性
+  otpWrapRef.value?.querySelectorAll('input').forEach((el) => {
+    el.setAttribute('inputmode', 'numeric')
+  })
+})
 </script>
 
 <template>
@@ -10,13 +19,16 @@ const appStore = useAppStore()
     <NCard class="w-300px rounded-lg text-center" :bordered="false">
       <div class="mb-16px text-48px">🔒</div>
       <div class="mb-16px text-18px font-700">请输入验证码</div>
-      <NInputOtp
-        block
-        :length="6"
-        :status="appStore.otpMsg ? 'error' : ''"
-        @updateValue="appStore.otpUpdate"
-        @finish="appStore.otpVerify"
-      />
+      <div ref="otpWrapRef">
+        <NInputOtp
+          block
+          :allow-input="(v) => !v || /^\d+$/.test(v)"
+          :length="6"
+          :status="appStore.otpMsg ? 'error' : ''"
+          @updateValue="appStore.otpUpdate"
+          @finish="appStore.otpVerify"
+        />
+      </div>
       <div class="min-h-20px mt-8px mb-16px text-12px text-[#d03050]">{{ appStore.otpMsg }}</div>
       <div class="text-12px text-gray-500">请联系管理员获取</div>
     </NCard>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   base: './',
   plugins: [vue(), UnoCSS()],
   server: {
+    host: true,
     proxy: {
       '/api/doubao': {
         target: 'https://ark.cn-beijing.volces.com',
